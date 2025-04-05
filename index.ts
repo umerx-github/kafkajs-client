a0df809641db4c712ff40803863585c1d4f6f75d
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-04-05 17:04:23 +0000

    Export at the top level

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-export * as Producer from './src/producer.js';
-export * as Consumer from './src/consumer.js';
-export * as IdempotentConsumerAdapter from './src/idempotentConsumerAdapter.js';
-export * as ReconnectingConsumerAdapter from './src/reconnectingConsumerAdapter.js'
-export * as ReconnectingProducerAdapter from './src/reconnectingProducerAdapter.js';
-export * as Interfaces from './src/interfaces.js';
+export * from './src/producer.js';
+export * from './src/consumer.js';
+export * from './src/idempotentConsumerAdapter.js';
+export * from './src/reconnectingConsumerAdapter.js'
+export * from './src/reconnectingProducerAdapter.js';
+export * from './src/interfaces.js';
